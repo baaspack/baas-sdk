5c9806c77d0cddacddfcfec9899be6696030efde
Holden Hinkle <holdenhinkle@gmail.com>
2020-04-16 12:03:32 -0400

    add websocket methods to support channels

--- ws.js
+++ ws.js
@@ -5,47 +5,91 @@ const createNewWebsocket = (url) => {
 }
 
 const wsFactory = (ws) => ({
+  findResource(collection, query) {
+    this.sendMessage({
+      action: 'find',
+      collection,
+      query,
+    });
+  },
   getCollection(collection) {
     this.sendMessage({
       action: 'getAll',
-      collection: collection,
+      collection,
     });
   },
   getResource(collection, id) {
     this.sendMessage({
       action: 'getOne',
-      collection: collection,
-      id: id,
+      collection,
+      id,
     });
   },
   createResource(collection, data) {
     this.sendMessage({
       action: 'create',
-      collection: collection,
-      data: data,
+      collection,
+      data,
     });
   },
   overwriteResource(collection, id, data) {
     this.sendMessage({
       action: 'update',
-      collection: collection,
-      id: id,
-      data: data,
+      collection,
+      id,
+      data,
     });
   },
   updateResource(collection, id, data) {
     this.sendMessage({
       action: 'patch',
-      collection: collection,
-      id: id,
-      data: data,
+      collection,
+      id,
+      data,
     });
   },
   deleteResource(collection, id) {
     this.sendMessage({
       action: 'delete',
-      collection: collection,
-      id: id,
+      collection,
+      id,
+    });
+  },
+  sendMessage(message) {
+    ws.send(JSON.stringify(message));
+  },
+  joinUsersChannels(usersInformationCollection) {
+    this.sendMessage({
+      action: 'joinUsersChannels',
+      usersInformationCollection,
+    });
+  },
+  joinChannel({ usersInformationCollection, channelType, channelId, channelName, creatorId }) {
+    this.sendMessage({
+      action: 'joinChannel',
+      usersInformationCollection,
+      channelType,
+      channelId,
+      channelName,
+      creatorId,
+    });
+  },
+  leaveChannel(usersInformationCollection, channelType, channelId) {
+    this.sendMessage({
+      action: 'leaveChannel',
+      usersInformationCollection,
+      channelType,
+      channelId,
+    });
+  },
+  changeChannel({ usersInformationCollection, channelType, channelId, channelName, creatorId }) {
+    this.sendMessage({
+      action: 'changeChannel',
+      usersInformationCollection,
+      channelType,
+      channelId,
+      channelName,
+      creatorId,
     });
   },
   open(message) {
@@ -56,18 +100,6 @@ const wsFactory = (ws) => ({
     message.action = 'close';
     this.sendMessage(message);
   },
-  sendMessage(message) {
-    ws.send(JSON.stringify(message));
-  },
-  // getCollectionsList() {
-  //   return sendRequest(`${url}/collections`)
-  //     .then((list) => console.log(list))
-  // },
-  // createNewCollection(collectionName) {
-  //   return sendRequest(`${url}/collections`, 'POST', { collectionName })
-  //     .then((coll) => console.log(coll));
-  // },
-
 });
 
 export default createNewWebsocket;
